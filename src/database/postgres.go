39fc35bbf3cdba208138d7d359db40c54d29fb1c
jrschmidtt <junior.schmidt.99@gmail.com>
2022-10-22 14:58:02 -0300

    Config Database

--- src/database/postgres.go
+++ src/database/postgres.go
@@ -1,1 +1,25 @@
 package database
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/jrschmidtt/api-gin/models"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+var (
+	DB *gorm.DB
+	err error
+)
+
+func ConnectPostgresDB(){
+	stringDeConexao := "host=localhost user=root password=root dbname=root port=5432 sslmode=disable"
+	DB, err = gorm.Open(postgres.Open(stringDeConexao))
+	if err != nil {
+		log.Fatal("Erro ao connectar com banco")
+	}
+	fmt.Println("Conectado com sucesso ao banco !")
+	DB.AutoMigrate(models.Aluno{})
+}
--- src/main.go
+++ src/main.go
@@ -1,11 +1,13 @@
 package main
 
 import (
+	"github.com/jrschmidtt/api-gin/database"
 	"github.com/jrschmidtt/api-gin/models"
 	"github.com/jrschmidtt/api-gin/routes"
 )
 
 func main() {
+	database.ConnectPostgresDB()
 	models.Alunos = []models.Aluno{
 		{Nome: "Aluno 1", CPF: "42942701013", RG: "47123453435"},
 		{Nome: "Aluno 2", CPF: "98679221074", RG: "47123453435"},
