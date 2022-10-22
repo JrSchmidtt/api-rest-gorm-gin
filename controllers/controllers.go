5635b458cb07a457207de58a73000930a15adace
jrschmidtt <junior.schmidt.99@gmail.com>
2022-10-22 14:29:40 -0300

    Model Aluno

--- controllers/controllers.go
+++ controllers/controllers.go
@@ -1,12 +1,12 @@
 package controllers
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/jrschmidtt/api-gin/models"
+)
 
 func ExibeTodosAlunos(c *gin.Context){
-	c.JSON(200, gin.H{
-		"id":"1",
-		"nome": "Junior Schmidt",
-	})
+	c.JSON(200, models.Alunos)
 }
 
 func HealthCheck(c *gin.Context){
