cfbb872535b1b283937494e29471e223906691e3
schmidt <junior.schmidt.99@gmail.com>
2022-10-24 21:29:47 -0300

    Render Html on GIN

--- src/controllers/controllers.go
+++ src/controllers/controllers.go
@@ -82,3 +82,15 @@ func FindAlunoByCPF(c *gin.Context) {
 	}
 	c.JSON(http.StatusOK, aluno)
 }
+
+func RenderIndex(c *gin.Context){
+	var alunos []models.Aluno
+	database.DB.Find(&alunos)
+	c.HTML(http.StatusOK, "index.html", gin.H{
+		"alunos": alunos,
+	})
+}
+
+func RotaNaoEncontrada(c *gin.Context){
+	c.HTML(http.StatusNotFound, "404.html", nil)
+}
--- src/routes/routes.go
+++ src/routes/routes.go
@@ -7,13 +7,17 @@ import (
 
 func HandleRequest(){
 	r := gin.Default()
+	r.LoadHTMLGlob("templates/*")
+	r.Static("/assets", "./assets")
 
+	r.GET("/", controllers.RenderIndex)
 	r.GET("/alunos", controllers.GetAllAluno)
 	r.POST("/alunos", controllers.CriaNovoAluno)
 	r.GET("/alunos/:id", controllers.FindAlunoById)
 	r.GET("/alunos/cpf/:cpf", controllers.FindAlunoByCPF)
 	r.DELETE("/alunos/:id", controllers.DeleteAluno)
 	r.PATCH("/alunos/:id", controllers.EditaAluno)
+	r.NoRoute(controllers.RotaNaoEncontrada)
 
 	r.Run(":5000")
 }
